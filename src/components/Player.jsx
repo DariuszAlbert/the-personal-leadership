3798dafd541232b27f9626cbdd70fef2da75caee
Dariusz Golunski <darek.golunski@gmail.com>
2023-02-06 08:39:56 +0100

    Add player responvive attempt 1

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -3,10 +3,17 @@ import styles from '../style';
 import ReactPlayer from "react-player/youtube";
 
 const Player = () => {
+    const width = '';
+    const height = '';
+
 
     return ( 
         <section id='player' className={` ${styles.flexCenter} flex-col relative z-20 mt-4 `}>
-            <div>
+            <div className="visible sm:hidden " >
+                <ReactPlayer className="visible sm:invisible " url='https://youtu.be/xIPS7QhdQlY' width={340} height={240}/>
+            </div>
+            
+            <div className="hidden sm:block " >
                 <ReactPlayer url='https://youtu.be/xIPS7QhdQlY' />
             </div>
     </section>
